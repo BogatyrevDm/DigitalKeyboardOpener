8cfd1ce046c9bb1c981a9c83a67331c95eb12e66
Bogatyrev Dmitrii <bogatyrevdm82@gmail.com>
2021-01-21 09:57:10 +0300

    adding button "Open calculator" and listener

--- app/src/main/java/com/example/digitalkeyboardopener/MainActivity.java
+++ app/src/main/java/com/example/digitalkeyboardopener/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.digitalkeyboardopener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonOpen = findViewById(R.id.button_open_calculator);
+        buttonOpen.setOnClickListener(v -> {
+            Uri uri = Uri.parse("calculator://open");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            ActivityInfo activityInfo = intent.resolveActivityInfo(getPackageManager(), intent.getFlags());
+            if (activityInfo != null) {
+                startActivity(intent);
+            }
+        });
     }
 }
